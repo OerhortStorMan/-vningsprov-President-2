fe8f725f4422f30df949dcff8a954b2fc0e656fa
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-13 10:31:02 +0200

    Init.

---
+++ PresidentGame/President.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace PresidentGame
+{
+    public class President
+    {
+        public Random generator = new Random();
+
+        public string name;
+
+        int environmentPriority = 0;
+        int defencePriority = 0;
+        int healthcarePriority = 0;
+
+        int corruption = 0;
+        int money = 0;
+
+        public President(string n)
+        {
+
+        }
+
+        public void Present()
+        {
+
+        }
+
+        public bool PassLegislation(int e, int d, int h, int bribe)
+        {
+
+        }
+
+    }
+}
---
+++ PresidentGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PresidentGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
